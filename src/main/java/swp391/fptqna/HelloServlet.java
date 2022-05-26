7b1de13a60c6d448881bf0a59944bbfce1c69c05
dnhaa <elnganha@gmail.com>
2022-05-26 14:22:12 +0700

    loi

--- src/main/java/swp391/fptqna/HelloServlet.java
+++ src/main/java/swp391/fptqna/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends HttpServlet {
 
     public void init() {
         message = "Hello World!";
-    }
+
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
