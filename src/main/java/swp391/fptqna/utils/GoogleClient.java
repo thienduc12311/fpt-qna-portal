7a1526658cb8511cb757f687d6b9e684e2130228
thienduc12311 <thienduc12311@gmail.com>
2022-06-02 16:50:49 +0700

    change gg client id

--- src/main/java/swp391/fptqna/utils/GoogleClient.java
+++ src/main/java/swp391/fptqna/utils/GoogleClient.java
@@ -1,9 +1,9 @@
 package swp391.fptqna.utils;
 
 public class GoogleClient {
-    public static String GOOGLE_CLIENT_ID = "439318717683-v1og8jquc6kn82nua7sun6cbvkt2oprb.apps.googleusercontent.com";
-    public static String GOOGLE_CLIENT_SECRET = "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";
-    public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/login-google";
+    public static String GOOGLE_CLIENT_ID = "987580920862-pdimkjajbgu8qoj80o3mt0bgsrgf53ti.apps.googleusercontent.com";
+    public static String GOOGLE_CLIENT_SECRET = "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";
+    public static String GOOGLE_REDIRECT_URI = "https://fpt-qna-portal.herokuapp.com/login-google";
     public static String GOOGLE_LINK_GET_TOKEN = "https://oauth2.googleapis.com/token";
     public static String GOOGLE_LINK_GET_USER_INFO = "https://openidconnect.googleapis.com/v1/userinfo?access_token=";
     public static String GOOGLE_GRANT_TYPE = "authorization_code";
