12c1f65de3ad5d4accd91adc26515a48c7e580e2
thienduc12311 <thienduc12311@gmail.com>
2022-06-02 17:05:19 +0700

    delete class for name

--- src/main/java/swp391/fptqna/utils/DButil.java
+++ src/main/java/swp391/fptqna/utils/DButil.java
@@ -11,7 +11,7 @@ public class DButil {
         String pass = "@@swp391";
         String connString = "jdbc:sqlserver://portalstudent.database.windows.net:1433;database=swp391;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
         //1
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         //2
         conn = DriverManager.getConnection(connString, user, pass);
         return conn;
