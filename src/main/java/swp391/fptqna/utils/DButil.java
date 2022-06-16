a9e86ada315e1dcca2d8d70a8fe9474547703581
ThienDuc <59289834+thienduc12311@users.noreply.github.com>
2022-06-16 10:38:51 +0700

    change conn string

--- src/main/java/swp391/fptqna/utils/DButil.java
+++ src/main/java/swp391/fptqna/utils/DButil.java
@@ -9,7 +9,7 @@ public class DButil {
         Connection conn = null;
         String user = "swpadmin";
         String pass = "@@swp391";
-        String connString = "jdbc:sqlserver://portalstudent.database.windows.net:1433;database=swp391;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+        String connString = "jdbc:sqlserver://qnaportal.database.windows.net:1433;database=QnAPortal1;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
         //1
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
